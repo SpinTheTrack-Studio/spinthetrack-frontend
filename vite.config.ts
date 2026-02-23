ea904dee98fce7fd7d4936f4e4f87d4f4aaf19b2
migz3rrr <miguel.joubert@epitech.eu>
2026-02-24 00:39:53 +0100

    feat(pwa): provide app as pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,43 @@
 import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import {VitePWA} from 'vite-plugin-pwa';
 
 export default defineConfig({
     plugins: [
         react(),
-        tailwindcss()
-    ],
-})
+        tailwindcss(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+            manifest: {
+                name: 'SpinTheTrack',
+                short_name: 'STT',
+                description: 'Le meilleur blindtest musical entre amis',
+                theme_color: '#0F0F13',
+                background_color: '#0F0F13',
+                display: 'standalone',
+                icons: [
+                    {
+                        src: '/logo-192.svg',
+                        sizes: '192x192',
+                        type: 'image/svg+xml'
+                    },
+                    {
+                        src: '/logo-512.svg',
+                        sizes: '512x512',
+                        type: 'image/svg+xml'
+                    },
+                    {
+                        src: '/logo-512.svg',
+                        sizes: '512x512',
+                        type: 'image/svg+xml'
+                    }
+                ]
+            },
+            workbox: {
+                globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}']
+            }
+        })
+    ]
+});
